4ddc6cfec840b8825224f88ac7747a15ba0799a7
shivanshiii-cloud <shivanshi.tripathi20104@gmail.com>
2026-08-01 22:44:39 +0530

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> result;
+
+        for(int i = 0; i <= n; i++) {
+            int num = i;          // copy of i
+            int count = 0;        // reset count
+
+            while(num != 0) {
+                num = num & (num - 1);  // remove last set bit
+                count++;                // count it
+            }
+
+            result.push_back(count);
+        }
+
+        return result;
+    }
+};
+// DP Solution 
+//class Solution {
+//public:
+   // vector<int> countBits(int n) {
+      //  vector<int> result(n + 1);
+
+       // for(int i = 1; i <= n; i++) {
+         //   result[i] = result[i & (i - 1)] + 1;
+        //}
+
+       // return result;
+//    //}
+//};
