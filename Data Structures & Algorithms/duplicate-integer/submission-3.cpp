7bfa012d3bcfc3a293291dec3f81486f6bc2510b
shivanshiii-cloud <shivanshi.tripathi20104@gmail.com>
2026-08-13 13:27:31 +0530

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int, int> freq;
+
+        for (int x : nums) {
+            freq[x]++;
+
+            if (freq[x] > 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
