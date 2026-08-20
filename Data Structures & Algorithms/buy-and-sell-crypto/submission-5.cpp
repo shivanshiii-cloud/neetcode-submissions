5d2b97c0807a233c029067eab90fb46ace30a095
shivanshiii-cloud <shivanshi.tripathi20104@gmail.com>
2026-08-20 23:55:25 +0530

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int gain = 0;
+
+        for(int i = 0; i < prices.size(); i++) {
+            for(int j = i + 1; j < prices.size(); j++) {
+
+                int total = prices[j] - prices[i];
+
+                gain = max(gain, total);
+            }
+        }
+
+        return gain;
+    }
+};
